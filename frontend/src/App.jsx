7a5a2672de3ad3477340a23be0c031587b7eedbd
Amandeep Singh <programmeramandeep@gmail.com>
2024-11-22 09:26:42 +0530

    feat: Update app structure with authentication handling, routing, and theme support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,50 @@
+import { Loader } from 'lucide-react';
+import { useEffect } from 'react';
+import { Toaster } from 'react-hot-toast';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Settings from './pages/Settings';
+import SignUp from './pages/SignUp';
+
+import { useAuthStore } from './store/useAuthStore';
+import { useThemeStore } from './store/useThemeStore';
+
 const App = () => {
-  return <div>Chat App</div>;
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { theme } = useThemeStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate:spin" />
+      </div>
+    );
+  }
+
+  return (
+    <div data-theme={theme}>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={authUser ? <Home /> : <Navigate to="/signin" />} />
+        <Route path="/signup" element={!authUser ? <SignUp /> : <Navigate to="/" />} />
+        <Route path="/signin" element={!authUser ? <Login /> : <Navigate to="/" />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/profile" element={authUser ? <Profile /> : <Navigate to="/login" />} />
+      </Routes>
+
+      <Toaster />
+    </div>
+  );
 };
 
 export default App;
