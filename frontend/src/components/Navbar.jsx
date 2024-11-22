cc3e68855dd57c29cd462aaadfdaec80576ca64a
Amandeep Singh <programmeramandeep@gmail.com>
2024-11-22 21:53:34 +0530

    feat: Implement responsive Navbar with authentication links and routing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,48 @@
+import { LogOut, MessageSquare, Settings, User } from 'lucide-react';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuthStore } from '../store/useAuthStore';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { authUser, signout } = useAuthStore();
+
+  return (
+    <header className="fixed top-0 z-40 w-full border-b bg-base-300 border-base-300 backdrop-blur-lg bg-base-100/80">
+      <div className="container h-16 px-4 mx-auto">
+        <div className="flex items-center justify-between h-full">
+          <div className="flex items-center gap-8">
+            <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+              <div className="flex items-center justify-center rounded-lg size-9 bg-primary/10">
+                <MessageSquare className="w-5 h-5 text-primary" />
+              </div>
+              <h1 className="text-lg font-bold">Chat App</h1>
+            </Link>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Link to={'/settings'} className="gap-2 transition-colors btn btn-sm">
+              <Settings className="w-4 h-4" />
+              <span className="hidden sm:inline">Settings</span>
+            </Link>
+
+            {authUser && (
+              <>
+                <Link to={'/profile'} className="gap-2 btn btn-sm">
+                  <User className="size-5" />
+                  <span className="hidden sm:inline">Profile</span>
+                </Link>
+
+                <button className="flex items-center gap-2" onClick={signout}>
+                  <LogOut className="size-5" />
+                  <span className="hidden sm:inline">Logout</span>
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
