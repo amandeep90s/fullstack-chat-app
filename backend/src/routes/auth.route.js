78f9edc27bdcb2152689f364a4ed69ed215fcd6b
Amandeep Singh <programmeramandeep@gmail.com>
2024-11-22 09:04:14 +0530

    fix: Correct middleware import path and update user retrieval in message route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { checkAuth, signin, signout, signup, updateProfile } from '../controllers/auth.controller.js';
-import { protectRoute } from '../middlewares/auth.middleware.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { getMessages, getUsers, sendMessage } from '../controllers/message.controller.js';
-import { protectRoute } from '../middlewares/auth.middleware.js';
+import { getMessages, getUsersForSidebar, sendMessage } from '../controllers/message.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
-router.get('/users', protectRoute, getUsers);
+router.get('/users', protectRoute, getUsersForSidebar);
 
 router.get('/:id', protectRoute, getMessages);
 
